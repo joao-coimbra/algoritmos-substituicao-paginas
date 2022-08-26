f6a14d6b57e5a82b3ad60d6c3b5edd7a47dbadd2
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-08-26 13:05:46 -0300

    add steps

--- src/components/Result/Table/index.jsx
+++ src/components/Result/Table/index.jsx
@@ -8,7 +8,14 @@ function Table({ collums, viewFails }) {
         <div className="h-table">
             <div className='content'>
                 {collums.map((collumn, index) => (
-                    <div key={index} className={(collumn.fail && viewFails) ? 'fail' : ''}>
+                    
+                    <div key={index} className={(collumn.fail && viewFails) ? 'fail' : ''}
+                        title={collumn.quadros.filter((este, i) => collumn.quadros.indexOf(este) === i).filter(arr => !arr).length
+                                    ? collumn.seq
+                                        ? `Página ${collumn.seq} já está na memória`
+                                        : null
+                                    : `Página ${collumn.seq} substitui a posição ${(parseInt(collumn.quadros.indexOf(collumn.seq))+1)} `}
+                    >
                         <div>{collumn.seq}</div>
                         {collumn.quadros.map((quadro, index) => (
                             <div key={index}
